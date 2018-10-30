4921efcdc3d8a8933ed2006cb1812fb43e8c9627
mldn <357038192@qq.com>
2018-10-30 18:40:46 +0800

    test Merger

--- hello.java
+++ hello.java
@@ -1,2 +1,2 @@
-public class hello{public static void main(String args[]){System.out .println("hello GIT !! ");System.out .println("hello GIT !! ");System.out .println("hello GIT !! ");System.out .println("hello GIT !! ");System.out.println("Java学习");System.out .println("朱乃超 ");}}
+public class hello{public static void main(String args[]){System.out .println("hello GIT !! ");System.out .println("hello GIT !! ");System.out .println("hello GIT !! ");System.out .println("hello GIT !! ");System.out.println("Java学习");System.out .println("朱乃超 ");System.out .println("朱乃超 ");System.out .println("朱乃超 ");}}
 

455f210c99f457816e917a1886990ba8ded237df
mldn <357038192@qq.com>
2018-10-31 09:47:15 +0800

    admin.java

--- Admin.java
+++ Admin.java
@@ -1,2 +1,2 @@
 
-public  class  Admin{	private List<Emp>emps;	private I		private String password;}
+public  class  Admin{	private List<Emp>emps;	private I		private String password;	private String IG;}
